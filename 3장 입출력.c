2ac32c50429e267a5721493acaed9ce1940f8134
hazechoii <chldkwls1@gmail.com>
2026-02-16 16:59:30 +0900

    Add files via upload

---
+++ 3장 입출력.c
@@ -0,0 +1,119 @@
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 1
+
+int main() {
+	int n;
+	scanf("%x", &n);
+
+	printf("decimal number = %d\n", n);
+	printf("octal number = %o", n);
+
+	return 0;
+} 
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 2
+
+int main() {
+	int w, h;
+	int area;
+
+	scanf("%d", &w);
+	scanf("%d", &h);
+
+	area = w * h;
+
+	printf("%d", area);
+
+	return 0;
+}
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 3
+
+int main() {
+	double w, h;
+	double area;
+
+	scanf("%lf", &w);
+	scanf("%lf", &h);
+
+	area = w * h;
+
+	printf("area = %.2f", area);
+
+	return 0;
+}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 4
+
+int main() {
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	printf("purchase 1 : %4d won\n", a);
+	printf("purchase 2 : %4d won\n", b);
+	printf("purchase 3 : %4d won\n", c);
+
+	return 0;
+}
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 5
+
+int main() {
+	double a;
+	int b;
+	scanf("%lf", &a);
+
+	b = a + 0.5;
+
+	printf("rounded up to %d", b);
+
+	return 0;
+}
+
+
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 6
+
+int main() {
+	int year;
+	int month;
+	int day;
+
+	scanf("%4d/%2d/%2d", &year, &month, &day);
+	printf("year %04d month %02d date %02d", year, month, day);
+	
+
+	return 0;
+}
+
+*/
