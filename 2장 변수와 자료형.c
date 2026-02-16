40f00cb9229c746a959e0e2addd32b177766adcc
hazechoii <chldkwls1@gmail.com>
2026-02-16 16:58:53 +0900

    Add files via upload

---
+++ 2장 변수와 자료형.c
@@ -0,0 +1,201 @@
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 1
+
+int main() {
+	int a;
+	int b;
+	int sum;
+	scanf("%d %d", &a, &b);
+
+	sum = a + b;
+
+	printf("sum: %d",sum);
+	
+
+
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 2
+
+int main() {
+	double a = 4.2;
+	double b = 5.1;
+	double sum;
+
+	sum = a + b;
+
+	printf("first number: %f\n", a);
+	printf("second number: %f\n", b);
+	printf("sum: %f", sum);
+
+	return 0;
+}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 3
+
+int main() {
+	int a;
+	char ch;
+	scanf("%d", &a);
+
+	ch = a;
+
+	printf("(int -> int) %d\n", a);
+	printf("(int -> char -> int) %d\n", ch);
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 4
+
+int main() {
+	int a;
+	unsigned char ch;
+	scanf("%d", &a);
+
+	ch = a;
+
+	printf("(int -> int) %d\n", a);
+	printf("(int -> unsigned char -> int) %d\n", ch);
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 5
+
+int main() {
+	printf("The double quotation mark is \". \n");
+	printf("The double quotation mark is '. \n");
+	printf("Character A is 'A'.\n");
+	printf("To represent a double quotation mark, use the escape sequence \\\". \n");
+
+	return 0;
+}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 6
+
+int main() {
+	char ch;
+	int a;
+	scanf("%c", &ch);
+
+	a =  ch - '0';
+
+	printf("(%%c) %c\n", ch);
+	printf("(%%d) %d", a);
+
+	return 0;
+}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 7
+
+int main() {
+	char ch;
+	int a;
+	scanf("%c", &ch);
+
+	a = ch - 'a';
+
+	printf("%c is the %dth lower case letter in the English alphabet.\n", ch, a);
+
+
+	return 0;
+}
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 8
+
+int main() {
+	char ch;
+	int a;
+	scanf("%d", &a);
+
+	ch = a + 'A';
+
+	printf("The %dth upper case letter in the English alphabet is %c\n",a, ch);
+
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 9
+
+int main() {
+	char ch;
+	char CH;
+	scanf("%c", &ch);
+
+	CH = ch - 'a' + 'A';
+	// CH = ch - 32;
+	// CH = ch ^ 32;
+
+	printf("%c", CH);
+
+
+	return 0;
+}
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 10
+
+int main() {
+	char ch;
+	char CH;
+	scanf("%c", &CH);
+
+
+	ch = CH - 'A' + 'a';
+	// ch = CH - 32;
+	// ch = CH ^ 32;
+
+	printf("%c", ch);
+
+	return 0;
+}
+
+*/
+
