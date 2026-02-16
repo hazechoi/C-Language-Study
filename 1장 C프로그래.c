c10535225e55c65735817b2a9aa498dea33dc919
hazechoii <chldkwls1@gmail.com>
2026-02-16 16:56:30 +0900

    Add files via upload

---
+++ 1장 C프로그래.c
@@ -0,0 +1,96 @@
+/*
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 1
+
+int main() {
+	printf("222222");
+	printf("2    2");
+	printf("2    2");
+	printf("222222");
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 2
+
+int main() {
+	printf("   A   ");
+	printf("  A A  ");
+	printf(" A   A ");
+	printf("A A A A");
+
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 3
+
+int main() {
+	int A;
+	scanf("%d", &A);
+
+	printf("%d%d%d%d%d%d\n",A,A,A,A,A,A);
+	printf("%d    %d\n",A,A);
+	printf("%d    %d\n",A,A);
+	printf("%d%d%d%d%d%d\n", A, A, A, A, A, A);
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 4
+
+int main() {
+	printf("My birthday is month 3 date 5");
+	return 0;
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 5
+
+int main() {
+	int a;
+	int b;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	printf("My birthday is month %d date %d\n", a, b);
+
+	return 0;
+}
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+// 6
+
+int main() {
+	int a;
+	int b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("My birthday is month %d date %d\n", a, b);
+
+	return 0;
+}
+
+
+*/
+
